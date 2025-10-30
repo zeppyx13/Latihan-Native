b2faf2507da0daf019a42b7cb34e5430d59e6cc5
Gung Nanda <gungnands@gmail.com>
2025-10-30 14:08:59 +0800

    icons

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Ionicons from '@react-native-vector-icons/ionicons';
+
+const icons = [
+  "cafe-outline",
+  "pizza-outline",
+  "fast-food-outline",
+  "heart-outline",
+  "location-outline",
+  "person-outline"
+] as const;
 
 const App = () => {
   return (
@@ -15,9 +25,10 @@ const App = () => {
 
           {/* Navigation */}
           <View style={styles.navigation}>
-            {[...Array(6)].map((_, i) => (
+            {icons.map((icon, i) => (
               <View key={i} style={styles.navItem}>
-                <View style={styles.navImage} />
+                {/* ICON GANTI NAVIMAGE */}
+                <Ionicons name={icon} size={50} color="#000000ff" />
                 <View style={styles.navLabel} />
               </View>
             ))}
@@ -60,10 +71,8 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#374151',
-  },
+  container: { flex: 1, backgroundColor: '#374151' },
+
   header: {
     height: 80,
     backgroundColor: '#374151',
@@ -72,11 +81,9 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   brandContainer: {
-    width: 80,
-    height: '100%',
-    backgroundColor: '#E5E7EB',
-    borderRadius: 100,
+    width: 80, height: '100%', backgroundColor: '#E5E7EB', borderRadius: 100,
   },
+
   navigation: {
     backgroundColor: '#E5E7EB',
     padding: 20,
@@ -90,22 +97,22 @@ const styles = StyleSheet.create({
     width: 90,
     height: 90,
     backgroundColor: '#9CA3AF',
-    justifyContent: 'space-between',
     borderRadius: 8,
     marginBottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  navImage: {
-    flex: 1,
-    backgroundColor: '#6B7280',
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
+  navIcon: {
+    marginTop: 8,
   },
   navLabel: {
-    height: 22,
+    width: "80%",
+    height: 6,
     backgroundColor: '#374151',
-    borderBottomLeftRadius: 8,
-    borderBottomRightRadius: 8,
+    borderRadius: 4,
+    marginTop: 6,
   },
+
   section: {
     backgroundColor: '#F3F4F6',
     paddingVertical: 15,
@@ -117,6 +124,7 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     color: '#111827',
   },
+
   orderItem: {
     flexDirection: 'row',
     backgroundColor: '#D1D5DB',
@@ -125,23 +133,16 @@ const styles = StyleSheet.create({
     borderRadius: 8,
   },
   orderItemImage: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#6B7280',
-    borderRadius: 8,
+    width: 60, height: 60, backgroundColor: '#6B7280', borderRadius: 8,
   },
   orderItemDesc: {
-    flex: 1,
-    backgroundColor: '#9CA3AF',
-    marginLeft: 10,
-    borderRadius: 8,
+    flex: 1, backgroundColor: '#9CA3AF', marginLeft: 10, borderRadius: 8,
   },
+
   bannerContainer: {
-    height: 180,
-    width: '100%',
-    borderRadius: 20,
-    backgroundColor: '#9CA3AF',
+    height: 180, width: '100%', borderRadius: 20, backgroundColor: '#9CA3AF',
   },
+
   foodItem: {
     flexDirection: 'row',
     backgroundColor: '#D1D5DB',
@@ -150,16 +151,10 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   foodItemImage: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#6B7280',
-    borderRadius: 8,
+    width: 60, height: 60, backgroundColor: '#6B7280', borderRadius: 8,
   },
   foodItemDesc: {
-    flex: 1,
-    backgroundColor: '#9CA3AF',
-    marginLeft: 10,
-    borderRadius: 8,
+    flex: 1, backgroundColor: '#9CA3AF', marginLeft: 10, borderRadius: 8,
   },
 });
 
