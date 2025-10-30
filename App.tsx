efc9ae6489659f0e616011d352a70d070476b70a
Gung Nanda <gungnands@gmail.com>
2025-10-30 12:38:36 +0800

    fisrt commit

--- App.tsx
+++ App.tsx
@@ -1,44 +1,150 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+      <SafeAreaView style={styles.container}>
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+        {/* Header */}
+        <View style={styles.header}>
+          <View style={styles.brandContainer} />
+        </View>
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+        {/* Navigation (1 kotak pink + 1 label) */}
+        <View style={styles.navigation}>
+          <View style={styles.navitem}>
+            <View style={styles.navimage} />
+            <View style={styles.navlabel} />
+          </View>
+        </View>
+
+        {/* Order section */}
+        <View style={styles.order}>
+          <Text>Order Section</Text>
+          <ScrollView style={{ flexDirection: 'row', gap: 20 }} horizontal={true}>
+            <View style={styles.orderItems}>
+              <View style={styles.orderItemImage} />
+              <View style={styles.orderItemDesc} />
+            </View>
+            <View style={styles.orderItems}>
+              <View style={styles.orderItemImage} />
+              <View style={styles.orderItemDesc} />
+            </View>
+            <View style={styles.orderItems}>
+              <View style={styles.orderItemImage} />
+              <View style={styles.orderItemDesc} />
+            </View>
+            <View style={styles.orderItems}>
+              <View style={styles.orderItemImage} />
+              <View style={styles.orderItemDesc} />
+            </View>
+            <View style={styles.orderItems}>
+              <View style={styles.orderItemImage} />
+              <View style={styles.orderItemDesc} />
+            </View>
+          </ScrollView>
+        </View>
+
+        {/* Favorite section */}
+        <View style={styles.favorite}>
+          <Text style={{ marginBottom: 10 }}>Favorite Section</Text>
+          <ScrollView contentContainerStyle={{ flexDirection: 'row', gap: 20 }} horizontal={true}>
+
+            <View style={styles.bannerContainer} />
+          </ScrollView>
+        </View>
+
+        {/* Food section */}
+        <View style={styles.food} />
+
+      </SafeAreaView >
+    </SafeAreaProvider >
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#1E293B',
+  },
+
+  header: {
+    height: 120,
+    backgroundColor: '#D62828',
+    padding: 10,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  brandContainer: {
+    width: 80,
+    height: '80%',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 100,
+  },
+  navigation: {
+    height: 140,
+    backgroundColor: '#FDE68A',
+    padding: 20,
+    justifyContent: 'center',
+  },
+  navitem: {
+    width: 90,
+    height: 90,
+    backgroundColor: '#BA68C8',
+  },
+  navimage: {
+    flex: 1,
+    backgroundColor: '#FF8A80',
+  },
+  navlabel: {
+    height: 22,
+    backgroundColor: '#1D4ED8',
+  },
+
+  order: {
+    height: 150,
+    backgroundColor: '#FDE68A',
+    padding: 10,
+  },
+  orderItems: {
+    flexDirection: 'row',
+    backgroundColor: 'teal',
+    padding: 10,
+  },
+  orderItemImage: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#F87171',
+  },
+  orderItemDesc: {
+    flex: 1,
+    backgroundColor: '#22C55E',
+  },
+
+  favorite: {
+    height: 120,
+    backgroundColor: '#FDE68A',
+    padding: 15,
+  },
+  bannerContainer: {
+    width: 200,
+    height: '100%',
+    backgroundColor: '#3B82F6',
+    marginBottom: 10,
+  },
+  favoriteItem: {
+    flex: 1,
+    backgroundColor: '#B91C1C',
+  },
+  food: {
+    height: 200,
+    backgroundColor: '#334155',
   },
 });
 
