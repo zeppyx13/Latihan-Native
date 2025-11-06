a18722a83d0b193f1fabeae2ea3ca831152082ce
Gung Nanda <gungnands@gmail.com>
2025-11-06 14:19:39 +0800

    header home off

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,13 @@ import ContactScreen from '../screen/Contact';
 import Food from '../screen/Food';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { HeaderShownContext } from '@react-navigation/elements';
 const stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
       <stack.Navigator>
-        <stack.Screen name="Home" component={HomeScreen} />
+        <stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
         <stack.Screen name="Contact" component={ContactScreen} />
         <stack.Screen name="Food" component={Food} />
       </stack.Navigator>
